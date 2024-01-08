89c38f3dba83e1f804f6e74f82e9e57805e5948d
Asfemi <pearlcstall@gmail.com>
2024-01-08 17:31:41 +0100

    timers screen refactored
    flutter bloc package added

--- lib/presentation/widgets/timers.dart
+++ lib/presentation/widgets/timers.dart
@@ -14,111 +14,125 @@ class Timers extends StatelessWidget {
     return ListView.builder(
         itemCount: _content.length,
         itemBuilder: (context, index) {
-          if (index == 0) {
-            return Padding(
+          return index == 0
+              ? _buildHeader(context)
+              : _buildTimerItem(context, index, _content);
+        });
+  }
+
+  Padding _buildTimerItem(
+      BuildContext context, int index, List<TimersData> _content) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 5.0),
+      child: InkWell(
+        onTap: () {
+          _navigateToTaskDetails(context, index);
+        },
+        child: OdooCard(
+            height: 136,
+            width: 361,
+            radius: 16,
+            child: Padding(
               padding: const EdgeInsets.symmetric(
                 vertical: 8.0,
                 horizontal: 10.0,
               ),
-              child: Text(
-                'You have 16 Timers',
-                style: Theme.of(context).textTheme.labelLarge,
-              ),
-            );
-          } else if (index != 0) {
-            return Padding(
-              padding: const EdgeInsets.only(bottom: 5.0),
-              child: InkWell(
-                onTap: () {
-                  //Navigator.pushNamed(context, '/TaskDetails');
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => TaskDetailsScreen(
-                              index: index,
-                            )),
-                  );
-                },
-                child: OdooCard(
-                    height: 136,
-                    width: 361,
-                    radius: 16,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 8.0,
-                        horizontal: 10.0,
-                      ),
-                      child: Row(children: [
-                        const SizedBox(
-                          height: 114,
-                          child: VerticalDivider(
-                            color: Color(0xffFFC629),
-                            width: 20,
-                            thickness: 2,
-                            indent: 2,
-                            endIndent: 2,
-                          ),
-                        ),
-                        const SizedBox(width: 5),
-                        SizedBox(
-                          height: 100,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Row(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Image.asset('lib/assets/icons/1.png'),
-                                  const SizedBox(width: 10),
-                                  SizedBox(
-                                    width: 180,
-                                    child: Text(
-                                      _content[index].name,
-                                      maxLines: 2,
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .titleMedium,
-                                    ),
-                                  )
-                                ],
-                              ),
-                              // const SizedBox(height: 6),
-                              Row(
-                                children: [
-                                  Image.asset('lib/assets/icons/3.png'),
-                                  const SizedBox(width: 10),
-                                  Text(
-                                    _content[index].details,
-                                    style:
-                                        Theme.of(context).textTheme.bodyMedium,
-                                  )
-                                ],
-                              ),
-                              // const SizedBox(height: 10),
-                              Row(
-                                children: [
-                                  Image.asset('lib/assets/icons/4.png'),
-                                  const SizedBox(width: 10),
-                                  Text(
-                                    _content[index].deadline,
-                                    style:
-                                        Theme.of(context).textTheme.bodyMedium,
-                                  )
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                        //const SizedBox(width: 5),
-                        const Spacer(),
-                        playPauseButton(index, context)
-                      ]),
-                    )),
-              ),
-            );
-          }
-          return null;
-        });
+              child: Row(children: [
+                SizedBox(
+                  height: 114,
+                  child: _buildVerticalDivider(),
+                ),
+                const SizedBox(width: 5),
+                _buildTimerItemContent(_content, index, context),
+                const Spacer(),
+                playPauseButton(index, context)
+              ]),
+            )),
+      ),
+    );
+  }
+
+  void _navigateToTaskDetails(BuildContext context, int index) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => TaskDetailsScreen(
+          index: index,
+        ),
+      ),
+    );
+  }
+
+  SizedBox _buildTimerItemContent(
+      List<TimersData> _content, int index, BuildContext context) {
+    return SizedBox(
+      height: 100,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.asset('lib/assets/icons/1.png'),
+              const SizedBox(width: 10),
+              SizedBox(
+                width: 180,
+                child: Text(
+                  _content[index].name,
+                  maxLines: 2,
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+              )
+            ],
+          ),
+          // const SizedBox(height: 6),
+          Row(
+            children: [
+              Image.asset('lib/assets/icons/3.png'),
+              const SizedBox(width: 10),
+              Text(
+                _content[index].details,
+                style: Theme.of(context).textTheme.bodyMedium,
+              )
+            ],
+          ),
+          // const SizedBox(height: 10),
+          Row(
+            children: [
+              Image.asset('lib/assets/icons/4.png'),
+              const SizedBox(width: 10),
+              Text(
+                _content[index].deadline,
+                style: Theme.of(context).textTheme.bodyMedium,
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  VerticalDivider _buildVerticalDivider() {
+    return const VerticalDivider(
+      color: Color(0xffFFC629),
+      width: 20,
+      thickness: 2,
+      indent: 2,
+      endIndent: 2,
+    );
+  }
+
+  Padding _buildHeader(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 8.0,
+        horizontal: 10.0,
+      ),
+      child: Text(
+        'You have 16 Timers',
+        style: Theme.of(context).textTheme.labelLarge,
+      ),
+    );
   }
 }
