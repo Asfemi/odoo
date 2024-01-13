a46c0379c5655a73d25fbc85cbe4287260cd343e
Asfemi <pearlcstall@gmail.com>
2024-01-13 23:58:08 +0100

    refactored the create timer screen

--- lib/presentation/screens/create_timer_screen.dart
+++ lib/presentation/screens/create_timer_screen.dart
@@ -7,7 +7,7 @@ import 'package:odoo/presentation/widgets/background.dart';
 import 'package:odoo/ticker.dart';
 
 class CreateTimer extends StatefulWidget {
-  const CreateTimer({super.key});
+  const CreateTimer({Key? key});
 
   @override
   State<CreateTimer> createState() => _CreateTimerState();
@@ -16,16 +16,17 @@ class CreateTimer extends StatefulWidget {
 class _CreateTimerState extends State<CreateTimer> {
   String? selectedProject;
   String? selectedTask;
-  List<String> projects = [
+  String timerDescription = '';
+  TimerBloc createTimerBloc = TimerBloc(ticker: const Ticker());
+  bool? isFavorite;
+   List<String> projects = [
     "Firebase Notifications",
     "Android development",
     "Flutter app development",
     "iOS app deployment"
   ];
   List<String> tasks = ["Riverpod", "Getx", "rxDart", "SO056 - Booqio V2"];
-  String timerDescription = '';
-  TimerBloc createTimerBloc = TimerBloc(ticker: const Ticker());
-  bool? isFavorite;
+  
 
   @override
   Widget build(BuildContext context) {
@@ -43,177 +44,182 @@ class _CreateTimerState extends State<CreateTimer> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          SizedBox(
-            height: 76,
-            child: DropdownButtonFormField<String>(
-              value: selectedProject,
-              icon: const ImageIcon(AssetImage('lib/assets/icons/icon.png')),
-              dropdownColor: Colors.blue,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(width: 1, color: Colors.grey.shade700),
-                  borderRadius: const BorderRadius.all(
-                    Radius.circular(8.0),
-                  ),
-                ),
-              ),
-              hint: const Text(
-                'Project',
-                style: TextStyle(fontSize: 18),
-              ),
-              onChanged: (value) {
-                setState(() {
-                  selectedProject = value!;
-                });
-              },
-              items: projects.map((project) {
-                return DropdownMenuItem<String>(
-                  value: project,
-                  child: Text(project),
-                );
-              }).toList(),
-            ),
+          _buildDropdown(
+            hint: 'Project',
+            value: selectedProject,
+            items: projects,
+            onChanged: (value) {
+              setState(() {
+                selectedProject = value;
+              });
+            },
           ),
           const SizedBox(height: 16.0),
-          SizedBox(
-            height: 76,
-            child: DropdownButtonFormField<String>(
-              value: selectedTask,
-              icon: const ImageIcon(AssetImage('lib/assets/icons/icon.png')),
-              dropdownColor: Colors.blue,
-              hint: const Text(
-                'Task',
-                style: TextStyle(fontSize: 18),
-              ),
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(width: 1, color: Colors.grey.shade700),
-                  borderRadius: const BorderRadius.all(
-                    Radius.circular(8.0),
-                  ),
-                ),
-              ),
-              onChanged: (value) {
-                setState(() {
-                  selectedTask = value!;
-                });
-              },
-              items: tasks.map((task) {
-                return DropdownMenuItem<String>(
-                  value: task,
-                  child: Text(task),
-                );
-              }).toList(),
-            ),
+          _buildDropdown(
+            hint: 'Task',
+            value: selectedTask,
+            items: tasks,
+            onChanged: (value) {
+              setState(() {
+                selectedTask = value;
+              });
+            },
           ),
           const SizedBox(height: 16.0),
-          SizedBox(
-            height: 76,
-            child: TextField(
-              onChanged: (value) {
-                setState(() {
-                  timerDescription = value;
-                });
-              },
-              maxLines: null,
-              decoration: InputDecoration(
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: const BorderRadius.all(Radius.circular(8)),
-                  borderSide: BorderSide(width: 1, color: Colors.grey.shade700),
-                ),
-                labelText: 'Description',
-              ),
-            ),
+          _buildTextField(
+            hint: 'Description',
+            onChanged: (value) {
+              setState(() {
+                timerDescription = value;
+              });
+            },
           ),
           const SizedBox(height: 16.0),
-          Row(
-            children: [
-              Checkbox(
-                value: isFavorite ?? false,
-                checkColor: Colors.white,
-                activeColor: Colors.grey,
-                onChanged: (value) {
-                  setState(() {
-                    isFavorite = value!;
-                  });
-                },
-              ),
-              Text(
-                'Make Favorite',
-                style: Theme.of(context).textTheme.bodyLarge,
-              ),
-            ],
+          _buildCheckbox(
+            value: isFavorite,
+            onChanged: (value) {
+              setState(() {
+                isFavorite = value;
+              });
+            },
           ),
           const Spacer(),
-          InkWell(
+          _buildButton(
             onTap: () {
-              if (selectedProject!.isNotEmpty &&
-                  selectedTask!.isNotEmpty &&
-                  timerDescription.isNotEmpty) {
-                TimersData newData = TimersData(
-                  name: selectedProject.toString(),
-                  details: selectedTask.toString(),
-                  deadline: '12/01/2024',
-                  isFav: isFavorite,
-                  taskdetails: TaskData(
-                    date: '01.01.2024',
-                    day: 'Monday',
-                    description: timerDescription.toString(),
-                    duration: '00:01:00',
-                    startTime: '12:00',
-                    assignedTo: 'New Assignee',
-                    deadline: '01/01/2025',
-                    project: 'New Project',
-                  ),
-                );
-                Helper.addTimersData(newData);
-              } else {
-                // Show a snackbar or any other validation message
-                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                  content: Text('Please fill in all fields.'),
-                ));
-              }
-
-              Navigator.pop(context);
+              _onCreateTimerButtonPressed(context);
             },
-            child: Ink.image(
-                height: 48,
-                width: 361,
-                image:
-                    const AssetImage('lib/assets/icons/createTimerButton.png')),
           ),
           const SizedBox(height: 16.0),
-          // ElevatedButton(
-          //   onPressed: () {
-          //     if (selectedProject.isNotEmpty && selectedTask.isNotEmpty && timerDescription.isNotEmpty) {
-          //       Timer newTimer = Timer(
-          //         project: selectedProject,
-          //         task: selectedTask,
-          //         description: timerDescription,
-          //         isFavorite: isFavorite,
-          //       );
-          //       widget.onTimerAdded(newTimer);
-          //     } else {
-          //       // Show a snackbar or any other validation message
-          //       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-          //         content: Text('Please fill in all fields.'),
-          //       ));
-          //     }
-          //   },
-          //   child: Text('Add Timer'),
-          // ),
         ],
       ),
     );
   }
 
+  Widget _buildDropdown({
+    required String hint,
+    required String? value,
+    required List<String> items,
+    required ValueChanged<String?> onChanged,
+  }) {
+    return SizedBox(
+      height: 76,
+      child: DropdownButtonFormField<String>(
+        value: value,
+        icon: const ImageIcon(AssetImage('lib/assets/icons/icon.png')),
+        dropdownColor: Colors.blue,
+        hint: Text(
+          hint,
+          style: const TextStyle(fontSize: 18),
+        ),
+        decoration: InputDecoration(
+          border: OutlineInputBorder(
+            borderSide: BorderSide(width: 1, color: Colors.grey.shade700),
+            borderRadius: const BorderRadius.all(
+              Radius.circular(8.0),
+            ),
+          ),
+        ),
+        onChanged: onChanged,
+        items: items.map((item) {
+          return DropdownMenuItem<String>(
+            value: item,
+            child: Text(item),
+          );
+        }).toList(),
+      ),
+    );
+  }
+
+  Widget _buildTextField({
+    required String hint,
+    required ValueChanged<String> onChanged,
+  }) {
+    return SizedBox(
+      height: 76,
+      child: TextField(
+        onChanged: onChanged,
+        maxLines: null,
+        decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+            borderRadius: const BorderRadius.all(Radius.circular(8)),
+            borderSide: BorderSide(width: 1, color: Colors.grey.shade700),
+          ),
+          labelText: hint,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCheckbox({
+    required bool? value,
+    required ValueChanged<bool?> onChanged,
+  }) {
+    return Row(
+      children: [
+        Checkbox(
+          value: value ?? false,
+          checkColor: Colors.white,
+          activeColor: Colors.grey,
+          onChanged: onChanged,
+        ),
+        Text(
+          'Make Favorite',
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildButton({
+    required VoidCallback onTap,
+  }) {
+    return InkWell(
+      onTap: onTap,
+      child: Ink.image(
+        height: 48,
+        width: 361,
+        image: const AssetImage('lib/assets/icons/createTimerButton.png'),
+      ),
+    );
+  }
+
+  void _onCreateTimerButtonPressed(BuildContext context) {
+    if (selectedProject?.isNotEmpty == true &&
+        selectedTask?.isNotEmpty == true &&
+        timerDescription.isNotEmpty) {
+      TimersData newData = TimersData(
+        name: selectedProject!,
+        details: selectedTask!,
+        deadline: '12/01/2024',
+        isFav: isFavorite,
+        taskdetails: TaskData(
+          date: '01.01.2024',
+          day: 'Monday',
+          description: timerDescription,
+          duration: '00:01:00',
+          startTime: '12:00',
+          assignedTo: 'New Assignee',
+          deadline: '01/01/2025',
+          project: 'New Project',
+        ),
+      );
+      Helper.addTimersData(newData);
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text('Please fill in all fields.'),
+      ));
+    }
+    Navigator.pop(context);
+  }
+
   AppBar _buildCreateTimerAppBar(BuildContext context) {
     return AppBar(
       leading: InkWell(
-          onTap: () {
-            Navigator.pop(context);
-          },
-          child: const ImageIcon(AssetImage('lib/assets/icons/Frame 300.png'))),
+        onTap: () {
+          Navigator.pop(context);
+        },
+        child: const ImageIcon(AssetImage('lib/assets/icons/Frame 300.png')),
+      ),
       backgroundColor: Colors.transparent,
       title: Text(
         'Create Timer',
